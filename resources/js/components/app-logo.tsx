80b0b4d1e630db43c4b871a08b83ca316f81f3be
Krisna <krisnaepras@gmail.com>
2025-08-12 21:29:47 +0700

    feat: enhance announcement creation and update functionality with image upload support and improved form handling

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -1,14 +1,17 @@
 import AppLogoIcon from './app-logo-icon';
+import { Church } from 'lucide-react';
 
 export default function AppLogo() {
-    return (
-        <>
-            <div className="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
-                <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
-            </div>
-            <div className="ml-1 grid flex-1 text-left text-sm">
-                <span className="mb-0.5 truncate leading-tight font-semibold">Laravel Starter Kit</span>
-            </div>
-        </>
-    );
+  return (
+    <div className="flex items-center">
+      <div className="p-2 bg-primary/10 rounded-full flex items-center justify-center">
+        <Church className="h-6 w-6 text-primary" />
+      </div>
+      <div className="ml-2 text-left">
+        <span className="truncate leading-tight font-semibold text-sm">
+          ST Paulus Kwimi
+        </span>
+      </div>
+    </div>
+  );
 }

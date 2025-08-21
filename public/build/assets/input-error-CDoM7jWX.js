e5b390a43d257b33b8de22d6251df4dbe8ad9030
Dias Norman <22082010134@student.upnjatim.ac.id>
2025-08-21 22:06:34 +0700

    push build

---
+++ public/build/assets/input-error-CDoM7jWX.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-C_7xbIZb.js";import{c as o}from"./createLucideIcon-CFNZbTRT.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};

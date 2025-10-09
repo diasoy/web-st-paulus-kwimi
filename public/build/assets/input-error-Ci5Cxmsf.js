6711c66bce335f33b9bdc934b99daccd9d9e3983
diasoy <diassnorrman@gmail.com>
2025-10-10 01:33:58 +0700

    Refactor code structure for improved readability and maintainability

--- public/build/assets/input-error-Ci5Cxmsf.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-CuhDvvSc.js";import{a as o}from"./createLucideIcon-BuYmdh1d.js";function a({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{a as I};
